d5bd553afda8c4039033301dc0214dacc98d63a3
Administrator <Administrator@DESKTOP-QB4O65F>
2023-01-03 03:48:00 +0300

    update i uploaed the wrong files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let map;
 let defalte={ lat: -34.397, lng: 150.644 };
 
-
+//get the map , can get par and not
 function initMap(first) {
   if (first)
   {
@@ -21,35 +21,43 @@ function initMap(first) {
 
 }
 //get the name from the searchbox
-function myFunction()
+function Search()
 {
-
+    LoadingSpinner();
 let querry = document.getElementById("searchBox").value; 
 
-if (querry)
+    if (querry)
+//make new object to create  request 
 { let request = new XMLHttpRequest();
       request.open("GET", "http://api.positionstack.com/v1/forward?access_key=82eb64b9129547181c780c53891dbe52&query="+querry);
     
       request.send();
-      request.onload = () => {
+      request.onload = () => { //lamdafunction to get the data and execute at the same time
         console.log(request);
         if (request.status === 200) {
           let locationInfo = JSON.parse(request.response).data;
         
-        if(locationInfo.length>0)
-        {
-        let location= locationInfo[0];
-          let payload= { lat: location.latitude, lng: location.longitude};
-          initMap(payload);
+            if (locationInfo.length > 0) {
+                let location = locationInfo[0];
+                let payload = { lat: location.latitude, lng: location.longitude };
+                initMap(payload);
+                hideSpinner();
             }
+            else {
+                alert("location not found");
+                hideSpinner();
+            }
+        
 
           // by default the response comes in the string format, we need to parse the data into JSON
           console.log(locationInfo);
-        } else {
+        } else
+        {
 
             console.log((`error ${request.status} ${request.statusText}`));
-         
-          }
+            alert("location not found");
+            hideSpinner();
+        }
 
       };
 }
@@ -59,11 +67,14 @@ if (querry)
 
 let mylet;
 
-function myFunction2() {
-    mylet = setTimeout(showPage, 3000);
+function LoadingSpinner()
+{
+    document.getElementById("Spinner").style.display = "block";
+    document.getElementById("Full").style.display = "none";
 }
 
-function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
+function hideSpinner()
+{
+    document.getElementById("Spinner").style.display = "none";
+    document.getElementById("Full").style.display = "block";
 }
